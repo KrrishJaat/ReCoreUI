3da85a66953bced242fd75255eed1e6566e13f88
Krrish Jaat <krrishjaat4763@outlook.com>
2026-04-02 09:10:28 +0530

    Update spoof.sh

--- recore/mods/spoofing/spoof.sh
+++ recore/mods/spoofing/spoof.sh
@@ -1,21 +1,11 @@
-# Device spoofing (S25 Ultra)
+########################################
+# Spoof To S25 Ultra
+########################################
 
-BPROP "system" "ro.product.device" "pa3q"
-BPROP "system" "ro.product.system.brand" "Samsung"
-BPROP "system" "ro.product.system.manufacturer" "Samsung"
-BPROP "system" "ro.product.product.manufacturer" "Samsung"
-BPROP "system" "ro.system.manufacturer" "Samsung"
-BPROP "system" "ro.product.model" "SM-S938B"
-BPROP "system" "ro.product.name" "Samsung S25 Ultra"
-BPROP "system" "ro.product.product.device" "pa3q"
-BPROP "system" "ro.product.product.model" "SM-S938B"
-BPROP "system" "ro.product.brand" "Samsung"
-BPROP "system" "ro.product.product.name" "Samsung S25 Ultra"
+LOG_INFO "Applying custom spoof props..."
 
-# Attestation spoof
+# System props
+BPROP "system" "ro.product.system.model" "SM-S938B"
+BPROP "system" "ro.product.system.name" "pa3qxxx"
 
-BPROP "system" "ro.product.brand_for_attestation" "samsung"
-BPROP "system" "ro.product.device_for_attestation" "pa3q"
-BPROP "system" "ro.product.manufacturer_for_attestation" "Samsung"
-BPROP "system" "ro.product.model_for_attestation" "SM-S938B"
-BPROP "system" "ro.product.name_for_attestation" "Samsung S25 Ultra"
+LOG_INFO "Custom spoof props applied!"
