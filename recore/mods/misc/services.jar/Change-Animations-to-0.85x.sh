2d944525e0b73927e118cb115a587374a68bc171
Krrish Jaat <krrishjaat4763@outlook.com>
2026-05-28 10:21:02 +0530

    Delete recore/mods/misc directory

--- recore/mods/misc/services.jar/Change-Animations-to-0.85x.sh
+++
@@ -1,8 +0,0 @@
-if grep -q "const/high16 v2, 0x3f800000" "$WMS_SMALI"; then
-    REPLACE_LINE \
-        "const/high16 v2, 0x3f800000" \
-        "const v2, 0x3f59999a    # 0.85f" \
-        "$WMS_SMALI"
-else
-    echo "⚠ Animation scale line not found, skipping patch"
-fi
