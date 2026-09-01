a18df18695e4c05e5f25d6477cedd9c6c1aa717d
Krrish Jaat <krrishjaat4763@outlook.com>
2026-09-01 14:40:20 +0530

    Update apktool.sh

--- scripts/internal/apktool.sh
+++ scripts/internal/apktool.sh
@@ -53,6 +53,7 @@ DEX_HEX_BYTES()
     fi
 }
 
+
 INSTALL_FRAMEWORK()
 {
     local FRAMEWORK_APK="$WORKSPACE/system/system/framework/framework-res.apk"
@@ -150,7 +151,7 @@ DECOMPILE()
 
         # Decompile with --no-src
         java -jar "$PREBUILTS/apktool/apktool.jar" d -api "$API" -f -j "$USABLE_THREADS" \
-            -o "$WORK_DIR" -p "$FRAMEWORK_DIR" -t "$SDK" -s "$FILE" || \
+            -o "$WORK_DIR" -p "$FRAMEWORK_DIR" -t "$SDK" -s "$FILE"
             ERROR_EXIT "Decompile failed"
 
         # Baksmali each dex parts
