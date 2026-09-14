6bd752f8c66bea8ca3d335afd91b187dac96a54d
Krrish Jaat <krrishjaat4763@outlook.com>
2026-09-14 09:51:37 +0530

    added app lock support

---
+++ recore/mods/applock/Add-AppLock-Feature.sh
@@ -0,0 +1,4 @@
+LOG_BEGIN "- Adding AppLock support from e1q"
+ADD_FROM_FW "a17" "system" "priv-app/AppLock/AppLock.apk"
+ADD_FROM_FW "a17" "system" "etc/permissions/privapp-permissions-com.samsung.android.applock.xml"
+LOG_END
