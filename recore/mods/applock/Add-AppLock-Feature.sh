15eb639230285168abb64e1a421cb2957dd311d3
Krrish Jaat <krrishjaat4763@outlook.com>
2026-09-21 08:12:17 +0530

    aod and rro

--- recore/mods/applock/Add-AppLock-Feature.sh
+++ recore/mods/applock/Add-AppLock-Feature.sh
@@ -1,4 +1,4 @@
-LOG_BEGIN "- Adding AppLock support from e1q"
+LOG_BEGIN "- Adding AppLock support"
 ADD_FROM_FW "a17" "system" "priv-app/AppLock/AppLock.apk"
 ADD_FROM_FW "a17" "system" "etc/permissions/privapp-permissions-com.samsung.android.applock.xml"
 LOG_END
