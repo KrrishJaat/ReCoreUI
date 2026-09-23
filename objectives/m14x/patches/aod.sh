2c65ed4745d5e8dc8f4ca8aadbf58e2cc2ac3ec5
Krrish Jaat <krrishjaat4763@outlook.com>
2026-09-23 18:45:40 +0530

    Update aod.sh

--- objectives/m14x/patches/aod.sh
+++ objectives/m14x/patches/aod.sh
@@ -1,5 +1,5 @@
 #This Device Has LCD Panel
 LOG_BEGIN "- Adding ClockPack_v80 and removing AODService_v80"
-ADD_FROM_FW "$BLOBS" "system" "priv-app/ClockPack_v80"
+ADD_FROM_FW "a17" "$BLOBS" "system" "priv-app/ClockPack_v80"
 SILENT REMOVE "system" "priv-app/AODService_v80"
 LOG_END
